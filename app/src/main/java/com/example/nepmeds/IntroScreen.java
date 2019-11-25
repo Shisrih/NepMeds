c5dc9699e7618bde9ab392ace31984e5f453539e
shishir <shishirbhandari1234@gmail.com>
2019-11-25 22:10:19 +0545

    Addition of a Splash screen(Introscreen) and Bottom Navigation Activity.

---
+++ app/src/main/java/com/example/nepmeds/IntroScreen.java
@@ -0,0 +1,30 @@
+package com.example.nepmeds;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class IntroScreen extends AppCompatActivity {
+    Handler handler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_introscreen);
+
+
+        handler = new Handler();
+
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent =  new Intent(IntroScreen.this,MainApp.class);
+                startActivity(intent);
+                finish();
+            }
+        },1700);
+    }
+}
